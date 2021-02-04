55a2331adcf16e018baf80790ddf54c63b3b34a2
Melanie Daoheuang <daoh0031@gmail.com>
2021-02-03 19:09:21 -0700

    Add code to order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,49 @@
+// Adds only up to three orders to the array
+function takeOrder(order, array) {
+  if (array.length < 3) {
+    array.push(order);  
+  }
+}
+
+// Removes order from array by order number 
+function refundOrder(num, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (num === array[i].orderNumber) {
+      array.splice(i, 1)
+    }
+  }
+}
+
+
+function listItems(order) {
+  var itemList = "";
+
+  // itemList += order[i].item;
+  for (var i = 0; i < order.length; i++) {
+    if (i < 0) {
+      itemList += `, ${order[i].item}` 
+    } else {
+      itemList += order[i].list
+    }
+  }
+  return itemList;
+}
+
+// Confirm if item is in the order array 
+function searchOrder(order, checkItem) {
+  var confirm = false;
+  
+  for (var i = 0; i < order.length; i++) {
+    if (order[i].item === checkItem) {
+      confirm = true;
+    } 
+  }
+  return confirm;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem
 }
