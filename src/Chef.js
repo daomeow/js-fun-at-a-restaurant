97bc94754e94380118c2b43a5b397c85820a457b
Melanie Daoheuang <daoh0031@gmail.com>
2021-02-05 16:44:31 -0700

    Complete Chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,34 @@
 class Chef {
-  constructor() {
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+    // this.isMorning = false;
+  }
+  greetCustomer(name, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+  checkForFood(foodItem) {
+    var type = foodItem.type;
+    var itemName = foodItem.name;
+    var confirm = false;
+
+    for (var i = 0; i < this.restaurant.menus[type].length; i++) {
+      if (itemName === this.restaurant.menus[type][i].name) {
+        confirm = true;
+      } else {
+        return false;
+      }
+    }
     
+    if (confirm) {
+      return `Yes, we're serving ${itemName} today!`
+    } else {
+      return `Sorry, we aren't serving ${itemName} today.`
+    }
   }
 }
 
--- src/order.js
+++ src/order.js
@@ -14,6 +14,7 @@ function refundOrder(num, array) {
   }
 }
 
+// Lists menu items by its name 
 function listItems(order) {
   var itemList = "";
 
